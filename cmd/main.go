4bfd60cf14d04f71cf1038e5f9ae45286b0fa2fd
Arsen Hovhannisyan <arsen1994t@gmail.com>
2026-02-19 16:49:19 +0400

    Graceful shutdown applied

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/arsenh/recipes-api/internal/app"
 	"github.com/arsenh/recipes-api/internal/config"
@@ -15,5 +21,27 @@ func main() {
 	app := app.New(cfg)
 	defer app.Close()
 
-	app.Router.Run(":8080")
+	server := &http.Server{
+		Addr:    cfg.Port,
+		Handler: app.Router, // Gin router is just http.Handler
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("run error: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
 }
